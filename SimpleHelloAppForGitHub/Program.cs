e3534fa13798152bd6d776bf4fd1509d6aa3bee8
sanandkumar <Anandkumar.Gandigudi@datavail.com>
2015-06-05 16:48:45 +0530

    Added ...

---
+++ SimpleHelloAppForGitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleHelloAppForGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple SetUp project Created");
+            System.Console.WriteLine("Press any key to exit...");
+            System.Console.ReadKey();
+        }
+    }
+}
