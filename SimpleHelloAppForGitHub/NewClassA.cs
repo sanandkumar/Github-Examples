6b83a01929de762b6ec837e45069be76151420ac
sanandkumar <Anandkumar.Gandigudi@datavail.com>
2015-06-05 17:32:32 +0530

    Added New Class file

---
+++ SimpleHelloAppForGitHub/NewClassA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleHelloAppForGitHub
+{
+    class NewClassA
+    {
+
+        public void GetData()
+        {
+            Console.WriteLine("New Class data Added ...");
+        }
+    }
+}
