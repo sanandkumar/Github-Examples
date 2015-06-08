533e2405d4058f122ec1984fbef2c41f040bc67d
sanandkumar <Anandkumar.Gandigudi@datavail.com>
2015-06-08 17:49:48 +0530

    Added New Class NewClass4

---
+++ SimpleHelloAppForGitHub/NewClass4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleHelloAppForGitHub
+{
+    class NewClass4
+    {
+        public void GetClass4Data()
+        {
+            Console.WriteLine("New Class data Added .....");
+        }
+    }
+}
